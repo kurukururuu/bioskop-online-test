db90bcfb47e86af1473c018c546752b40b2b0d24
Krisna Fathurahman <krisna.fathurahman@gmail.com>
2021-10-28 13:23:41 +0700

    feat(payment/calculate): calculate price

--- store/payment.js
+++ store/payment.js
@@ -1,11 +1,19 @@
 const INITIAL_STATE = {
   paymentMethods: [],
+  selectedPaymentMethod: null,
+  calculation: {},
 }
 
 export const state = () => INITIAL_STATE
 
 export const mutations = {
   SET_PAYMENT_METHODS: (state, payload) => {
+    state.paymentMethods = payload
+  },
+  SET_SELECTED_PAYMENT_METHOD: (state, payload) => {
+    state.selectedPaymentMethod = payload
+  },
+  SET_CALCULATION: (state, payload) => {
     state.paymentMethods = payload
   },
 }
@@ -32,6 +40,32 @@ export const actions = {
         }
       )
       commit('SET_PAYMENT_METHODS', response.data)
+    } catch (err) {
+      console.error({ err })
+    }
+  },
+  selectPaymentMethod({ commit }, { code }) {
+    commit('SET_SELECTED_PAYMENT_METHOD', code)
+  },
+  async calculatePrice(
+    { commit },
+    { code, idTitle, idSeries, idPackage, voucherCode }
+  ) {
+    commit('SET_CALCULATION', null)
+    try {
+      const response = await this.$axios.$post(
+        `${process.env.API_URL}/payment/calculate`,
+        {
+          code,
+          additionals: {
+            id_title: idTitle,
+            id_series: idSeries,
+            id_package: idPackage,
+            voucher_code: voucherCode,
+          },
+        }
+      )
+      commit('SET_CALCULATION', response.data)
     } catch (err) {
       console.error({ err })
     }
